d59a0cead32e39e0927f45ae4e6e043d5df7fedd
jpark2320 <jpark2320@gmail.com>
2020-03-21 17:59:43 -0400

    Set up a basic cycle of a user joining a chat room

--- client/src/components/Chat/index.js
+++ client/src/components/Chat/index.js
@@ -1,14 +1,15 @@
-import React, { useState, useEffect } from "react";
-import queryString from "query-string";
-import io from "socket.io-client";
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
 let socket;
 
 const Chat = ({ location }) => {
-  const [name, setName] = useState("");
-  const [room, setRoom] = useState("");
-
-  const ENDPOINT = "localhost:5000";
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  const ENDPOINT = 'localhost:5000';
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
@@ -18,12 +19,42 @@ const Chat = ({ location }) => {
     setName(name);
     setRoom(room);
 
-    socket.emit("join", { name, room }, ({ error }) => {
-      alert(error);
-    });
+    socket.emit('join', { name, room }, () => {});
+
+    return () => {
+      socket.emit('disconnect');
+
+      socket.off();
+    };
   }, [ENDPOINT, location.search]);
 
-  return <h1>Chat</h1>;
+  useEffect(() => {
+    socket.on('message', message => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
+
+  const sendMessage = e => {
+    e.preventDefault();
+
+    if (message) {
+      socket.emit('sendMessage', message, () => setMessage(''));
+    }
+  };
+
+  console.log(message, messages);
+
+  return (
+    <div className='outerContainer'>
+      <div className='container'>
+        <input
+          value={message}
+          onChange={e => setMessage(e.target.value)}
+          onKeyPress={e => (e.key === 'Enter' ? sendMessage(e) : null)}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
